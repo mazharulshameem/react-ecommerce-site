c7e0390776a6c56c12944f260d6cbe95bdf4a5ce
Mazharul Shameem <mazharulis.cs.07@gmail.com>
2022-09-26 17:53:32 +0600

    cart updated

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import './Cart.css'
 
-const Cart = ({cart}) => {
+const Cart = (props) => {
+  const {cart} = props;
+  let price = 0;
+  let shipping =0;
+  let quantity = 0;
+  for ( const product of cart){
+    price = price + product.price * product.quantity;
+    shipping = shipping + product.shipping;
+    quantity = quantity + product.quantity;
+  }
+  const tax = price*0.1 ;
+  const grandTotal = price + shipping + tax;
   return (
-    <div>
+    <div className="cart">
       <h3>Order Summery in Cart</h3>
-      <p>selected item:{cart.length}</p>
+      <div className='cart-item'>
+        <p>Selected Item: {quantity}</p>
+        <p>Total Price: ${price}</p>
+        <p>Total Shipping Charge: ${shipping}</p>
+        <p>Total Tax: ${tax.toFixed(2)} </p>
+        <h5>Grand Total: ${grandTotal .toFixed(2)} </h5>
+        <div button-two>
+          <button> Order Clear</button>
+          <button> Order Review</button>
+        </div>
+      </div>
     </div>
   );
 };
